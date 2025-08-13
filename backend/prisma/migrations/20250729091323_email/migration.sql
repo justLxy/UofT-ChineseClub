8cdf4c3ef01c67e3d55bd3f7547e60de88cba0b8
SijiaXie_Scarlett <scarlett8120@foxmail.com>
2025-08-13 20:04:39 +0800

    取消跟踪文件

--- backend/prisma/migrations/20250729091323_email/migration.sql
+++
@@ -1,6 +0,0 @@
--- AlterTable
-ALTER TABLE "Staff" ADD COLUMN     "isEmailVerified" BOOLEAN NOT NULL DEFAULT false,
-ADD COLUMN     "lastVerificationCodeSent" TIMESTAMP(3),
-ADD COLUMN     "verificationCode" TEXT,
-ADD COLUMN     "verificationCodeAttempts" INTEGER NOT NULL DEFAULT 0,
-ADD COLUMN     "verificationCodeExpiry" TIMESTAMP(3);
